530f74ff01d889b6ec96ccba735b5f4311a5b4b2
Pooneh <p.moghadda30@gmail.com>
2023-06-23 21:57:18 +0200

    amended

--- srcs/requirements/mariadb/conf/init.sql
+++ srcs/requirements/mariadb/conf/init.sql
@@ -4,14 +4,3 @@ CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED BY 'secret';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'wordpress';
-
--- some refs
--- for command help: http://g2pc1.bu.edu/~qzpeng/manual/MySQL%20Commands.htm
--- for login: 
--- docker exec -it mariadb bash
--- mysql; (will not work)
--- mysql -u wordpress -psecret;
--- show databases;
--- use wordpress;
--- show tables;
--- exit;
